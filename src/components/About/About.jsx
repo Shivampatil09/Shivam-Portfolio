e0ecd1c1c7e6bb884f7b1e3675a07898f0d8d912
Shivam Patil <147076070+Shivampatil09@users.noreply.github.com>
2026-04-07 20:23:46 +0530

    Update 1.1

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -51,7 +51,8 @@ const About = () => {
           <a
           
           //  href="https://drive.google.com/file/d/1HfhF4O4ISuVIj2FDq63UwkHbDVwwOfaM/view?usp=sharing"
-            href="https://drive.google.com/file/d/18D_iMdR-8Ldp1KSosYnum0lCFzoPzI72/view?usp=drive_link"
+           // href="https://drive.google.com/file/d/18D_iMdR-8Ldp1KSosYnum0lCFzoPzI72/view?usp=drive_link"
+            href="https://drive.google.com/file/d/18D_iMdR-8Ldp1KSosYnum0lCFzoPzI72/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
